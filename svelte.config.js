af8e970c18684001e95ac7b1c01a31804c9a972b
emilyhunt <emily.hunt.physics@gmail.com>
2025-12-19 14:15:52 +0100

    Add github pages build workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md']
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx', '.md'],
+	preprocess: [mdsvex(mdsvexOptions)],
+	kit: {
+		adapter: adapter({ fallback: '404.html' }),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
