22b68653c659a86d0923988d9667007f70f7ec8b
emilyhunt <emily.hunt.physics@gmail.com>
2026-01-23 11:22:41 +0100

    Better about us page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,23 @@
+<script>
+	import { siteMailingListSignup } from '$lib/config';
+</script>
+
 <h1>About us</h1>
 
 <p>
 	<em><strong>The Star Formation Newsletter</strong></em> has been the premier destination for news about
-	star formation since its founding in 1992. We're pleased to announce that 2026 marks its return after
-	a hiatus.
+	star and planet formation since its founding in 1992.
 </p>
+
+<p>
+	You can <a href={siteMailingListSignup} target="_blank">sign up to our mailing list</a> to receive our monthly updates.
+</p>
+
+<p>The newsletter is currently maintained by:</p>
+
+<ul>
+	<li><strong>João Alves:</strong> Editor-in-Chief</li>
+	<li><strong>Emily Hunt:</strong> Technical Editor</li>
+	<li><strong>Moa Huppenkothen:</strong> Editor</li>
+	<li><strong>Lilly Kormann:</strong> Editor</li>
+</ul>
