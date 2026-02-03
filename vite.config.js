16b3bc08a7eb5697842872aa2ebd06a344db03f7
emilyhunt <emily.hunt.physics@gmail.com>
2026-02-03 10:38:12 +0100

    Hotfix to editorial mode (buggy features behind an env var)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
+// Enable fancy animation in local editorial mode
+// Currently sometimes broken (see #11)
+let serverWatchParams = {};
+if (process.env.PRETTY_EDITORIAL_MODE) {
+	console.log('Pretty editorial mode enabled! Warning: may not work on some machines right now.');
+	serverWatchParams = {
+		server: {
+			watch: {
+				ignored: ['**/hidden.json']
+			}
+		}
+	};
+}
+
 export default defineConfig({
 	plugins: [
 		sveltekit(),
@@ -9,9 +23,5 @@ export default defineConfig({
 			compiler: 'svelte'
 		})
 	],
-	server: {
-		watch: {
-			ignored: ["**/hidden.json"]
-		}
-	}
+	...serverWatchParams
 });
