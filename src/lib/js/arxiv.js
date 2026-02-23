243985d9a375e42af551398afb1613014368a823
Emily Hunt <emily.hunt.physics@gmail.com>
2026-02-23 10:19:27 +0100

    Add more prints

--- src/lib/js/arxiv.js
+++ src/lib/js/arxiv.js
@@ -1,14 +1,20 @@
 // import arxivClient from 'arxiv-client';
-
 import arxivClient, { abstract, and, not, or, title, category } from 'arxiv-client';
 
 export async function fetchArXivMetadata(paperIDs) {
+	console.log('\n-----------------------');
+	console.log('FETCHING METADATA FROM ARXIV');
+	console.log('-----------------------');
+
 	if (paperIDs.length > 1000) {
 		throw new Error(
 			`More than 1000 paper IDs - this is too many to query. Something is wrong! (IDs: ${paperIDs.length})`
 		);
 	}
 	const client = arxivClient.default;
+
+	console.log = function() {}
+
 	let articles = await client
 		.ids(paperIDs)
 		.start(0)
@@ -16,6 +22,8 @@ export async function fetchArXivMetadata(paperIDs) {
 		// .sortBy('submittedDate')
 		// .sortOrder('ascending')
 		.execute();
+
+	console.log("Success! (hopefully.)")
 
 	articles = addAdditionalMetadata(articles);
 
@@ -27,10 +35,12 @@ export async function fetchArXivMetadata(paperIDs) {
 }
 
 function addAdditionalMetadata(articles) {
+	console.log("Adding additional metadata...")
+	
 	articles.forEach((obj) => {
 		obj.idShort = obj.id.split('/').slice(-1)[0].replaceAll(/v.*/g, '');
 		delete obj.links;
-		delete obj.doi;  // Since many don't have it
+		delete obj.doi; // Since many don't have it
 	});
 	return articles;
 }
