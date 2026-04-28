8c4efc927754150aac65bccd8395d8b423e2ea0a
emilyhunt <emily.hunt.physics@gmail.com>
2026-04-28 12:17:34 +0200

    Fixes #12 - "Recent News" now does not show current article and is limited to last month

--- src/lib/components/articles/RecentNews.svelte
+++ src/lib/components/articles/RecentNews.svelte
@@ -4,14 +4,31 @@
 
 	// Passed props include all blog info (like title, date, etc).
 	let props = $props();
+
+	const thisNewsletter = $derived(new Date(props.date));
+
+	async function getLastNewsletter() {
+		const posts = await getPosts({
+			limit: 1,
+			category: 'newsletters',
+			excludedURLs: props.url,
+			maxDate: thisNewsletter
+		});
+		return posts.posts[0].date;
+	}
 </script>
 
 <div class="other-news">
-	<h2 style="margin-top: 75px; text-align: center"><em>― Recent other news ―</em></h2>
-	{#await getPosts({ page: 1, limit: 3 })}
+	<h2 style="margin-top: 75px; text-align: center"><em>― Since the previous SFN ―</em></h2>
+	<!-- Todo: this double-await is ugly! -->
+	{#await getLastNewsletter()}
 		<p>Loading...</p>
-	{:then sfnPosts}
-		<PostList posts={sfnPosts.posts} />
+	{:then lastNewsletter}
+		{#await getPosts( { page: 1, limit: 10, excludeHiddenPosts: true, excludedURLs: props.url, minDate: lastNewsletter, maxDate: thisNewsletter, badCategories: 'newsletters' } )}
+			<p>Loading...</p>
+		{:then sfnPosts}
+			<PostList posts={sfnPosts.posts} />
+		{/await}
 	{:catch error}
 		<p>Error: {error.message}</p>
 	{/await}
