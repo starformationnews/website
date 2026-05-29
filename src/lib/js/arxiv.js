716b3196d9a81a3f5acee5d4293ac32af69e679d
emilyhunt <emily.hunt.physics@gmail.com>
2026-05-29 17:11:28 +0200

    Bugfix to console logging

--- src/lib/js/arxiv.js
+++ src/lib/js/arxiv.js
@@ -13,30 +13,46 @@ export async function fetchArXivMetadata(paperIDs) {
 	}
 	const client = arxivClient.default;
 
-	console.log = function() {}
+	// Block console.log because arxiv-api spams it
+	const old_log = console.log;
+	console.log = function () {};
 
-	let articles = await client
-		.ids(paperIDs)
-		.start(0)
-		.maxResults(paperIDs.length)
-		// .sortBy('submittedDate')
-		// .sortOrder('ascending')
-		.execute();
+	let articles;
 
-	console.log("Success! (hopefully.)")
+	try {
+		articles = await client
+			.ids(paperIDs)
+			.start(0)
+			.maxResults(paperIDs.length)
+			// .sortBy('submittedDate')
+			// .sortOrder('ascending')
+			.execute();
+	} catch (error) {
+		console.error(error);
+		throw new Error(
+			'Unable to download arXiv posts. There are a few possible reasons.\n' +
+				'  1. Check your internet connection.\n' +
+				'  2. Re-run with --dry-run and check whether the queried IDs look valid.\n' +
+				'  3. Check that the arXiv api (export.arxiv.org) is working: https://status.arxiv.org/\n' +
+				"  4. If the URL 'https://export.arxiv.org/api/query?search_query=&max_results=10' " +
+				"returns with 'Rate exceeded' in your web browser, then someone on your network " +
+				'has got you IP banned from arXiv... try querying on a different network.\n' +
+				'If you still encounter issues, please contact the maintainer of the SFN.'
+		);
+	}
+
+	// Unblock console.log
+	console.log = old_log;
+	console.log('Success! (hopefully.)');
 
 	articles = addAdditionalMetadata(articles);
 
 	return articles;
-
-	// const articlesObject = new Object();
-	// articles.forEach((obj, index) => (articlesObject[obj.idShort] = obj));
-	// return articlesObject;
 }
 
 function addAdditionalMetadata(articles) {
-	console.log("Adding additional metadata...")
-	
+	console.log('Adding additional metadata...');
+
 	articles.forEach((obj) => {
 		obj.idShort = obj.id.split('/').slice(-1)[0].replaceAll(/v.*/g, '');
 		delete obj.links;
